da6f947c46f8e6bc5930bf9e27f6a09a62abf42a
harry <harryharding97@gmail.com>
2023-04-22 14:25:14 +0100

    Removed unnecessary service variable

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions
 import time
 
-service = Service("C:/Users/harry/OneDrive/Documents/Coding/Python_course/chromedriver_win32/chromedriver.exe")
 options = ChromeOptions()
 
 options.add_experimental_option("detach", True)
